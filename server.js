cf33f5f926c4fe4c07b887edd4187813c13d0ffc
Juebele <eronuebele@gmail.com>
2023-05-07 21:19:59 -0400

    added .sql file and changed server port

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
-const PORT = process.env.PORT || 3001;
+
+
+const PORT = process.env.PORT || 3306;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// const db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'classlist_db',
-//   },
-//   console.log(`Connected to the classlist_db database.`)
-// );
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employees_db',
+  },
+  console.log(`Connected to the employees_db database.`)
+);
 
 // db.query('SELECT * FROM students', function (err, results) {
 //   console.log(results);
